150c339252240b4c404316e7bd40634b4f85f3fa
marvellouseromosele <88911227+marvellouseromosele@users.noreply.github.com>
2021-11-08 20:03:14 +0100

    Add files via upload

--- users.js
+++ users.js
@@ -1,28 +1,28 @@
-const express = require('express');
-const router = express.Router;
+// const express = require('express');
+// const router = express.Router;
 
-const users = [{name: 'Marvellous Eromosele', email: 'emarvellous204@gmail.com'}]
+// const users = [{name: 'Marvellous Eromosele', email: 'emarvellous204@gmail.com'}]
  
-router.get('/', (_, res) => {
-    res.send('My Express App');
-});
+// router.get('/', (_, res) => {
+//     res.send('My Express App');
+// });
 
-router.get('/users', (_, res) => {
-    res.json({ ok: true, users });
-});
+// router.get('/users', (_, res) => {
+//     res.json({ ok: true, users });
+// });
 
-router.get('/user/:name', (req, res) => {
-const { name } = req.params;
-const user = users.filter((user) => user.name === name)[0];
-res.json({ok: true, user});
-});
+// router.get('/user/:name', (req, res) => {
+// const { name } = req.params;
+// const user = users.filter((user) => user.name === name)[0];
+// res.json({ok: true, user});
+// });
 
-router.post('/adduser', (req, res) => {
-const { name, email} = req.body;
-if (name && email) {
-    users.push({ name, email });
-    res.json({ ok: true, users });
-}
-}); 
+// router.post('/adduser', (req, res) => {
+// const { name, email} = req.body;
+// if (name && email) {
+//     users.push({ name, email });
+//     res.json({ ok: true, users });
+// }
+// }); 
 
-module.exports = router;
+// module.exports  = router;
